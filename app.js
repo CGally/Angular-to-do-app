cb12d1d5d94d5a9f762152910856882c906ebd58
CGally <32678174+CGally@users.noreply.github.com>
2017-11-10 16:12:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ function ToDoController() {
   this.createTask = () => {
     this.tasks.push(this.taskName);
     this.taskName = '';
+  }
   this.deleteTask = (index) => {
     this.tasks.splice(index, 1);
-  }
   }
 };
